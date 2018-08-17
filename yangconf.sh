86dce47137ac34b306bd8e36331a14071a9d7b1f
Xavier A <25086112+xav-a@users.noreply.github.com>
2018-08-17 00:50:44 -0500

    Ejemplo finalizado

--- yangconf.sh
+++ yangconf.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo -e "\nESCENARIO - Data Model Maquina de Turing\n"
 
-echo "Modelo YANG\n"
+echo -e "Modelo YANG\n"
 
 cat turing-machine.yang
 
@@ -12,15 +12,17 @@ echo -e "\n"
 
 echo -e "Estructura del modelo\n"
 
-venv/Scripts/pyang -f tree turing-machine.yang
+pyang -f tree turing-machine.yang
 
 echo -e "\n"
 read -p "*********************" -n 1 -s
 echo -e "\n"
 
-echo  -e "Esqueleto de Instancia XML\n"
+echo -e "Esqueleto de Instancia XML\n"
 
-venv/Scripts/pyang -f sample-xml-skeleton --sample-xml-skeleton-doctype=config turing-machine.yang
+pyang -f sample-xml-skeleton --sample-xml-skeleton-doctype=config turing-machine.yang
+
+read -n 1 -s
 
 echo -e "\nInstancia\n"
 cat turing-machine-config.xml
@@ -29,5 +31,26 @@ echo -e "\n"
 read -p "*********************" -n 1 -s
 echo -e "\n"
 
-venv/Scripts/yang2dsdl -t config turing-machine.yang
+yang2dsdl -t config turing-machine.yang
 
+echo -e "\n Validando Instancia \n"
+
+yang2dsdl -s -j -b turing-machine -t config -v turing-machine-config.xml
+
+echo -e "\n*********************\n"
+read -p "Enviando RPC 'initialize'" -n 1 -s
+echo -e "\n"
+
+yang2dsdl -t rpc -v initialize-rpc.xml turing-machine.yang
+
+echo -e "\n"
+read -p "Enviando RPC 'run'" -n 1 -s
+echo -e "\n\n"
+read -p "Esperando por 'halted' (notificacion)" -n 1 -s
+echo -e "\n\n"
+read -p "Requermiento 'get'" -n 1 -s
+echo -e "\n\n"
+
+echo -e "'get-reply':\n"
+
+cat turing-machine-get-reply.xml
